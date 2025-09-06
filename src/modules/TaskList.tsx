021e35580fd6130cc4531c1434c391951bda7c80
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-09-06 19:35:25 +0300

    feat: update task list

--- src/modules/TaskList.tsx
+++ src/modules/TaskList.tsx
@@ -1,23 +1,37 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Empty } from "src/components/Empty";
-import { List } from "src/components/List";
-import { deleteTask, tasksSelector, toggleTask } from "src/store/taskSlice";
+import { useDispatch, useSelector } from 'react-redux'
+import { Empty } from 'src/components/Empty'
+import { FilterButton } from 'src/components/FilterButton'
+import { List } from 'src/components/List'
+import { TaskCounter } from 'src/components/TaskCounter'
+import {
+  deleteTask,
+  filteredTasksSelector,
+  toggleTask,
+} from 'src/store/taskSlice'
 
 export const TaskList = () => {
-  const items = useSelector(tasksSelector);
-  const dispatch = useDispatch();
+  const items = useSelector(filteredTasksSelector)
+  const dispatch = useDispatch()
 
-  const handleDelete = (id: Task["id"]) => {
-    dispatch(deleteTask(id));
-  };
+  const handleDelete = (id: Task['id']) => {
+    dispatch(deleteTask(id))
+  }
 
-  const handleToggle = (id: Task["id"]) => {
-    dispatch(toggleTask(id));
-  };
+  const handleToggle = (id: Task['id']) => {
+    dispatch(toggleTask(id))
+  }
 
-  return items.length > 0 ? (
-    <List items={items} onDelete={handleDelete} onToggle={handleToggle} />
-  ) : (
-    <Empty />
-  );
-};
+  return (
+    <div className='task-list-container'>
+      <div className='task-list-controls'>
+        <FilterButton />
+        <TaskCounter />
+      </div>
+      {items.length > 0 ? (
+        <List items={items} onDelete={handleDelete} onToggle={handleToggle} />
+      ) : (
+        <Empty />
+      )}
+    </div>
+  )
+}
